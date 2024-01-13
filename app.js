50beae12d65c08d55ca559a388c36ed10f366cfa
Amandeep Singh <amandeepsingh@Amandeeps-MacBook-Pro.local>
2024-01-13 23:20:07 +0530

    file syste commit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hello")
+const fs = require('fs');
+
+const textIn=fs.readFileSync('sampleText.txt','utf-8');
+console.log(textIn)
+
+// const hello ="hello world"
+// console.log(hello)
