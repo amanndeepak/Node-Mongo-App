1ea04f3e3324560aa7b74768d4d4ad88a3ab3f7d
Amandeep Singh <amandeepsingh@Amandeeps-MacBook-Pro.local>
2024-01-14 23:58:37 +0530

    14th feb code change

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 const http = require('http');
-
-
-
+const url = require('url')
 //  server 
 const server = http.createServer((req,res)=>{
-    console.log(req.data)
-    res.end("Hello from the server !");
-   
+    console.log(req.url)
+ const pathName = req.url
+
+    if(pathName == '/overview'){
+        res.end("THIS IS OVERVIEW")
+    }else if(pathName == '/product'){
+        res.end("THIS IS PRODUCTS")
+    }else if(pathName == '/mydata'){
+        fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8', (err,data)=>{
+            const myselfData = JSON.parse(data);
+            res.writeHead(200 , {'content-type':'application/json'})
+            res.end(data)
+        })
+       // res.end("API")
+    }
+    else {
+        res.writeHead(404 , { 'Content-type': 'text/html' , 'my-own-header' :'hello world'});
+        res.end('<h1>Page not Found ! </h1>');
+    }
+// res.end("Hello from the server !");
 })
 
 server.listen(8000,'127.0.0.1', ()=>
